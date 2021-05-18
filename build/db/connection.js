f5d1e8341d8df2f1f09c4a07b29fc6beb8643cf8
David Salguero García <nebirous@hotmail.com>
2021-05-18 15:49:59 +0200

    delete build

--- build/db/connection.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize('nodeTS', 'root', '1235648Aa', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
-exports.default = db;
